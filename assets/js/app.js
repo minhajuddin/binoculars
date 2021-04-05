d6629ebf8366e5bc1a89db08b911abe255702284
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2021-04-04 21:20:43 -0400

    Execute query on ctrl+enter

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,19 @@ import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  metadata: {
+    keyup: (e, el) => {
+      return {
+        key: e.key,
+        metaKey: e.metaKey,
+        ctrlKey: e.ctrlKey
+      }
+    }
+  }
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
--- lib/binoculars_web/live/page_live.ex
+++ lib/binoculars_web/live/page_live.ex
@@ -1,25 +1,53 @@
 defmodule BinocularsWeb.PageLive do
   use BinocularsWeb, :live_view
+  require Logger
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, pid: self() |> inspect()), temporary_assigns: [columns: [], rows: [], number_of_rows: 0] }
+    {:ok, assign(socket, query: "", error: nil),
+     temporary_assigns: [columns: [], rows: [], number_of_rows: 0]}
   end
 
   @impl true
   def handle_event("execute", %{"query" => query}, socket) do
-    result_set = Ecto.Adapters.SQL.query!(
-      Binoculars.Repo, query, []
-    )
-    IO.puts "EXECUTING #{ query }"
-    {:noreply, assign(socket, columns: result_set.columns, rows: result_set.rows, number_of_rows: result_set.num_rows)
-    }
+    {:noreply, execute_and_assign(socket, query)}
+  end
+
+  def handle_event(
+        "execute",
+        %{"ctrlKey" => ctrl, "key" => "Enter", "metaKey" => meta, "value" => query},
+        socket
+      )
+      when ctrl or meta do
+    {:noreply, execute_and_assign(socket, query)}
   end
 
   def handle_event(event, params, socket) do
-    Logger.info 
-    {:noreply, assign(socket, columns: result_set.columns, rows: result_set.rows, number_of_rows: result_set.num_rows)}
+    Logger.info(code: "UNHANDLED", event: event, params: params)
+    {:noreply, socket}
   end
 
+  defp execute_and_assign(socket, query) do
+    try do
+      result_set =
+        Ecto.Adapters.SQL.query!(
+          Binoculars.Repo,
+          query,
+          []
+        )
 
+      assign(socket,
+        query: query,
+        columns: result_set.columns,
+        rows: result_set.rows,
+        number_of_rows: result_set.num_rows,
+        error: nil
+      )
+    rescue
+      ex in Postgrex.Error ->
+        assign(socket,
+          error: ex.message
+        )
+    end
+  end
 end
